728c3d90eec6c87652bf05fe77e50ee7cfa93f0e
Jamshid Tashpulatov <44395036+JamshidTashpulatov@users.noreply.github.com>
2021-09-07 16:48:31 +0500

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: 'Poppins',
+      },
+      colors: {
+        primary: '#7B61FF',
+        success: '#33C390',
+        info: '#3865F3',
+        danger: '#FF4F60',
+        warning: '#FFC042'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
